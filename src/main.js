d42231285284d8f8a52a59038e3a543e99dd9d9a
Jonalogy <jonalogy@gmail.com>
2017-06-29 10:43:43 +0800

    npm installed redux

--- src/main.js
+++ src/main.js
@@ -1,8 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { App } from './containers/index.js'
+import { createStore } from 'react-redux'
+
+const store = createStore(todoApp)
+
+
 
 ReactDOM.render(
-  (<App />),
+ <Provider store={store}>
+ <App />
+</Provider>,
   document.getElementById('main')
 )
